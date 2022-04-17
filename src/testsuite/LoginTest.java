330e120f9a66f1913523daa0ce87e3fc9125ade6
glebe9ju <hetal.savalia@gmail.com>
2022-04-17 23:24:53 +0100

    Selenium Homework week 11

---
+++ src/testsuite/LoginTest.java
@@ -0,0 +1,107 @@
+package testsuite;
+/*userShouldNavigateToLoginPageSuccessfully
+
+        * click on the ‘Login’ link
+        * Verify the text ‘Welcome, Please Sign
+        In!’*/
+
+import browserfactory.BaseTest;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class LoginTest extends BaseTest {
+    String baseUrl = "https://demo.nopcommerce.com/";
+
+    @Before
+    public void setUp() {
+        openBrowser(baseUrl);
+    }
+
+    @Test
+    public void userShouldNavigateToLoginPageSuccessfully() {
+// Find login link and click on login link
+        WebElement loginLink = driver.findElement(By.linkText("Log in"));
+        loginLink.click();
+//welcome page title by classname by locator
+        WebElement welcomeMessage = driver.findElement(By.className("page-title"));
+        // Actual Message of welcome page
+        String actualWelcomeMessage = welcomeMessage.getText();
+// ExpectedMessage
+        String expectedMessage = "Welcome, Please Sign In!";
+//validate Actual and Expected Message
+        Assert.assertEquals("Wrong Welcome Message", expectedMessage, actualWelcomeMessage);
+
+    }
+
+    /*
+    userShouldLoginSuccessfullyWithValidCredentials
+
+* click on the ‘Login’ link
+* Enter valid username
+* Enter valid password
+* Click on ‘LOGIN’ button
+* Verify the ‘Log out’ text is display
+     */
+
+    @Test
+    public void userShouldLoginSuccessfullyWithValidCredentials() {
+        // Find login link and click on login link
+        WebElement loginLink = driver.findElement(By.linkText("Log in"));
+        loginLink.click();
+//Find username  locator by id provide value by sendskeys
+        WebElement userName = driver.findElement(By.id("Email"));
+        userName.sendKeys("hey123@yahoo.com");
+        // Find password locator by name, provide value by sendkeys
+        WebElement userPassWord = driver.findElement(By.name("Password"));
+        userPassWord.sendKeys("hey123");
+        //
+        WebElement userLogIn = driver.findElement(By.xpath("//button[@class='button-1 login-button']"));
+        userLogIn.click();
+
+        WebElement userLogOut = driver.findElement(By.className("ico-logout"));
+
+        Assert.assertEquals("text not display", "Log out", userLogOut.getText());
+
+    }
+
+    /*
+    verifyTheErrorMessage
+
+* click on the ‘Login’ link
+* Enter invalid username
+* Enter invalid password
+* Click on Login button
+* Verify the error message ‘Login was unsuccessful.
+Please correct the errors and try again. No customer account found’
+     */
+    @Test
+    public void verifyTheErrorMessage() {
+        // Find login link and click on login link
+        WebElement loginLink = driver.findElement(By.linkText("Log in"));
+        loginLink.click();
+//Find username  locator by id provide value by sendskeys
+        WebElement userName = driver.findElement(By.id("Email"));
+        userName.sendKeys("prime123@yahoo.com");
+        // Find password locator by name, provide value by sendkeys
+        WebElement userPassWord = driver.findElement(By.name("Password"));
+        userPassWord.sendKeys("Prime123");
+        //
+        WebElement userLogIn = driver.findElement(By.xpath("//button[@class='button-1 login-button']"));
+        userLogIn.click();
+
+        WebElement errorMessage = driver.findElement(By.xpath("//div[@class='message-error validation-summary-errors']"));
+        String expectedMessage = "Login was unsuccessful. Please correct the errors and try again.\nNo customer account found";
+        Assert.assertEquals("text not display", expectedMessage, errorMessage.getText());
+
+
+    }
+
+    @After
+    public void tearDown() {
+        closeBrowsers();
+    }
+}
